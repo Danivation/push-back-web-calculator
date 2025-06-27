9b02069228fc78bdfc1bb2a87426c1cc03f9863f
Daniel <divelasco7@gmail.com>
2025-06-26 19:22:49 -0500

    cache update

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "offline-cache-v3";
+const CACHE_NAME = "offline-cache-v4";
 const OFFLINE_FILES = [
     "/push-back-web-calculator/",
     "/push-back-web-calculator/index.html",
